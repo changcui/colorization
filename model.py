3896435be11ecf28fa087e3f175cb2a1310039db
ssxy00 <user@shenxinyideMacBook-Pro.local>
2019-06-23 20:49:09 +0800

    modify model

---
+++ model.py
@@ -0,0 +1,91 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+
+class LowLevelFeatNet(nn.Module):
+    def __init__(self):
+        super(LowLevelFeatNet, self).__init__()
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=2, padding=1)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(256)
+        self.conv5 = nn.Conv2d(256, 256, kernel_size=3, stride=2, padding=1)
+        self.bn5 = nn.BatchNorm2d(256)
+        self.conv6 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
+        self.bn6 = nn.BatchNorm2d(512)
+       
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = F.relu(self.bn4(self.conv4(x)))
+        x = F.relu(self.bn5(self.conv5(x)))
+        x = F.relu(self.bn6(self.conv6(x)))
+        return x
+
+
+class MidLevelFeatNet(nn.Module):
+    def __init__(self):
+        super(MidLevelFeatNet, self).__init__()
+        self.conv1 = nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(512)
+        self.conv2 = nn.Conv2d(512, 256, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(256)
+
+    def forward(self, x):
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        return x
+
+
+class ColorizationNet(nn.Module):
+    def __init__(self):
+        super(ColorizationNet, self).__init__()
+        self.conv1 = nn.Conv2d(256, 128, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.conv2 = nn.Conv2d(128, 64, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 32, kernel_size=3, stride=1, padding=1)
+        self.bn5 = nn.BatchNorm2d(32)
+        self.conv5 = nn.Conv2d(32, 2, kernel_size=3, stride=1, padding=1)
+        self.upsample = nn.UpsamplingNearest2d(scale_factor=2)
+
+    def forward(self, x):
+        x = F.relu(self.bn2(self.conv1(x)))
+        x = self.upsample(x)
+        x = F.relu(self.bn3(self.conv2(x)))
+        x = F.relu(self.bn4(self.conv3(x)))
+        x = self.upsample(x)
+        x = F.sigmoid(self.bn5(self.conv4(x)))
+        x = self.upsample(self.conv5(x))
+        return x
+
+
+class ColorNet(nn.Module):
+    def __init__(self):
+        super(ColorNet, self).__init__()
+        self.down_net = LowLevelFeatNet()
+        self.mid_net = MidLevelFeatNet()
+        self.up_net = ColorizationNet()
+
+    def forward(self, x):
+        x = self.down_net(x)
+        x = self.mid_net(x)
+        x = self.up_net(x)
+        return x
+
+
+if __name__ == "__main__":
+
+    colornet = ColorNet()
+    data = torch.ones(3, 1, 224, 224)
+    output = colornet(data)
+    print(output)
+    print(output.shape)
