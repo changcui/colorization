a9db7bf7215acf227a8589c4608b7d0115f0bce8
changcui <cuichang147@gmail.com>
2019-06-23 19:10:07 +0800

    fixed some bugs in train

--- train.py
+++ train.py
@@ -11,8 +11,8 @@ train_loader = torch.utils.data.DataLoader(train_set, batch_size=cfg.batch_size,
                 shuffle=True, num_workers=4)
 
 model = ColorNet()
-if os.path.exists('weights/weights.pkl'):
-    model.load_state_dict(torch.load('weights/weights.pkl'))
+if os.path.exists('Weights/weights.pkl'):
+    model.load_state_dict(torch.load('Weights/weights.pkl'))
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
@@ -26,16 +26,16 @@ def train():
         for batch_idx, (data, label) in enumerate(train_loader):
             l, ab = data.to(device), label.to(device)
             output = model(l)
-            loss = torch.pow((output, ab), 2).sum() / output.numel()
+            loss = torch.pow(output - ab, 2).sum() / output.numel()
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            if batch_idx % 100 == 0:
+            if batch_idx % cfg.save_period == 0:
                 print('Train Epoch:%d\tPercent:[%d/%d (%.0f%%)]\tLoss:%.9f\n' % (
                     epoch, batch_idx * data.size(0), len(train_loader.dataset),
                     100. * batch_idx / len(train_loader), loss.item()))
-                torch.save(model.state_dict(), 'weights/weights.pkl')
-    torch.save(model.state_dict(), 'weights/weights.pkl')
+                torch.save(model.state_dict(), 'Weights/weights.pkl')
+    torch.save(model.state_dict(), 'Weights/weights.pkl')
 
 if __name__ == '__main__':
     train()
